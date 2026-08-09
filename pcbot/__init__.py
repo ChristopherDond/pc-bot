35229f9e485389c1d0b52350cb157183cfc28e52
ChristopherDond <christopherdondici@gmail.com>
2026-08-09 15:33:51 -0300

    chore: bootstrap project scaffold (pyproject, gitignore, package init)

---
+++ pcbot/__init__.py
@@ -0,0 +1,13 @@
+import ctypes
+
+__version__ = "0.1.0"
+
+
+def enable_dpi_awareness():
+    try:
+        ctypes.windll.shcore.SetProcessDpiAwareness(2)
+    except Exception:
+        try:
+            ctypes.windll.user32.SetProcessDPIAware()
+        except Exception:
+            pass
